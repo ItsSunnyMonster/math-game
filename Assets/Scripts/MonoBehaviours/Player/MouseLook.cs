4a98db771fdadf37202696f7b680155bd4aa51ed
SunnyMonster123 <tonylin12345@outlook.com>
2021-03-03 21:00:19 +1300

    Completed player movement (2020.2.6f1)

---
+++ Assets/Scripts/MonoBehaviours/Player/MouseLook.cs
@@ -0,0 +1,38 @@
+//
+// Copyright (c) SunnyMonster
+// https://www.youtube.com/channel/UCbKQHYlzpR_pa5UL7JNP3kg/
+//
+
+using UnityEngine;
+
+namespace MonoBehaviours.Player
+{
+	public class MouseLook : MonoBehaviour
+	{
+		public float mouseSensitivity = 100f;
+
+		private float _xRotation = 0f; //keeping track of the x rotation so that we can clamp it afterwards
+
+		public Transform playerBody; //transform of the player object
+
+		private void Start()
+		{
+			Cursor.lockState = CursorLockMode.Locked; //lock the cursor
+		}
+
+		private void Update()
+		{
+			//get mouse position input
+			var mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+			var mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+			//set the x rotation and clamp it
+			_xRotation -= mouseY;
+			_xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+			
+			//rotate according to the input
+			transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+			playerBody.Rotate(Vector3.up * mouseX);
+		}
+	}
+}
---
+++ Assets/Scripts/MonoBehaviours/Player/PlayerMovement.cs
@@ -0,0 +1,53 @@
+//
+// Copyright (c) SunnyMonster
+// https://www.youtube.com/channel/UCbKQHYlzpR_pa5UL7JNP3kg/
+//
+
+using UnityEngine;
+
+namespace MonoBehaviours.Player
+{
+	public class PlayerMovement : MonoBehaviour
+	{
+		public CharacterController controller;
+		public Transform groundCheck;
+		public LayerMask groundMask;
+		
+		private Vector3 _yVelocity;
+		
+		public float speed = 12f;
+		public float groundDistance = 0.4f;
+		public float gravity = -19.62f;
+		public float jumpHeight = 3f;
+
+		private bool _isGrounded;
+
+		private void Update()
+		{
+			//do ground check
+            _isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+            if (_isGrounded && _yVelocity.y < 0)
+            {
+         		_yVelocity.y = -2f;
+            }
+            
+			//get input
+			var x = Input.GetAxis("Horizontal");
+			var z = Input.GetAxis("Vertical");
+			
+			//move the player based on input
+			var move = transform.right * x + transform.forward * z;
+			controller.Move(move * (speed * Time.deltaTime)); 
+
+			//check for jumping input and jump if button down
+			if (Input.GetButtonDown("Jump") && _isGrounded)
+			{
+				_yVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+			}
+			
+			//move the player based on gravity
+			_yVelocity.y += gravity * Time.deltaTime; 
+			controller.Move(_yVelocity * Time.deltaTime);
+		}
+	}
+}
